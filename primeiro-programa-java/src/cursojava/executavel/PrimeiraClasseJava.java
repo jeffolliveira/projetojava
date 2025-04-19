1f372db61aaf698bca7c5c099632eb78849ed96e
Jeferson <Jeferson@JEFFOLLIVEIRA>
2025-04-19 18:16:34 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*Main é um método auto executavel em java*/
+	public static void main(String[] args) {
+		
+		//escreve texto no console
+		System.out.println("meu primeiro programa em java");
+		System.out.println("Java é orientado a objetos");
+	}
+
+}
